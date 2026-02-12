631cb5ae895e2b681c75cff8f624a80f5346e857
Bigillow <tsunheil@nycstudents.net>
2026-02-12 09:24:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
-import json
-## Open the JSON file of pokemon data
-pokedex = open("./pokedex.json", encoding="utf8")
-## create variable "data" that represents the enitre pokedex list
-data = json.load(pokedex)
-print(data[0])
+# import json
+# ## Open the JSON file of pokemon data
+# pokedex = open("./pokedex.json", encoding="utf8")
+# ## create variable "data" that represents the enitre pokedex list
+# data = json.load(pokedex)
+# print(data[8])
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
 
+
+
+# import json
+# pokedex = open("./pokedex.json", encoding="utf8")
+# data = json.load(pokedex)
+# def pick():
+#     for poke in data:
+#         print(poke['name']['english'])
+# pick()
+
+
+
 # Add a language choice feature and print the pokemons name based on the user input
 
+
+
+# import json
+# pokedex = open("./pokedex.json", encoding="utf8")
+# data = json.load(pokedex)
+# language = input("Pick a language")
+# def pick():
+#     for poke in data:
+#         print(poke['name'][language])
+# pick()
+
+
+
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
+
+
+
+j
+
+
 
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
